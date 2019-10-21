294ee4dfb4b84327589a5c0bec841cc83480b3f0
Miguel A. Martinez <miguel.emezero@gmail.com>
2019-10-21 16:47:38 -0500

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://miguelzero:0N9h2rF9HGW5FTzg@cluster0-jdrqa.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URL;
 }
 
 
